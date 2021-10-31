14aa0bf25f8222da3909e29b8ea1d9dc372ec50a
G4brielG <gabriel240901@gmail.com>
2021-10-30 22:15:03 -0300

    a

--- src/components/pages/Posts.js
+++ src/components/pages/Posts.js
@@ -2,40 +2,58 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const Posts = () => {
-  const [data, setData] = useState([]);
+  const [datos, setDatos] = useState([]);
 
-  const fetchData = async () => {
+  const obtenerDatos = async () => {
     try {
       const data = await fetch(
         "https://equipo-server.herokuapp.com/estudiante/"
       );
       const res = await data.json();
-      handleRenderData(res);
+      setDatos(res)
+      console.log(res)
     } catch (error) {
       console.log("Hubo un error en la petición. " + error);
     }
   };
 
-  const handleRenderData = (res) => {
-    setData(res[0].datos_personales.nombre_apellido);
-    res.map((e) => console.log(e));
-    // <p key={i}>{e.datos_personales.nombre_apellido}</p>
-  };
-
   useEffect(() => {
-    fetchData();
-  });
+    obtenerDatos();
+  }, []);
   return (
     <>
-      <div>
-        <label>Nombre:</label>
-        <br></br>
-        <input type="text" value={data}></input>
-        <br></br>
-        <label>correo:</label>
-        <br></br>
-        <input type="text" value={data}></input>
-      </div>
+      <ul>
+        {
+          datos.map(item => (
+            <div key={item._id}>
+              <label>---Datos Personales---</label>
+              <br></br>
+              <label>{item._id}</label>
+              <br></br>
+              <label>Nombre</label>
+              <br></br>
+              <input type="text" key={item.datos_personales.nombre_apellido} defaultValue={item.datos_personales.nombre_apellido}></input>
+              <br></br>
+              <label>edad</label>
+              <br></br>
+              <input type="number"  key={item.datos_personales.edad} defaultValue={item.datos_personales.edad}></input>
+              <br></br>
+              <label>DNI</label>
+              <br></br>
+              <input type="number"  key={item.datos_personales.dni} defaultValue={item.datos_personales.dni}></input>
+              <br></br>
+              <label>correo</label>
+              <br></br>
+              <input type="text"  key={item.datos_personales.correo} defaultValue={item.datos_personales.correo}></input>
+              <br></br>
+              <label>nombre de usuario</label>
+              <br></br>
+              <input type="text"  key={item.userId.nombre_usuario} defaultValue={item.userId.nombre_usuario}></input>
+              <br></br>
+            </div>
+          ))
+        }
+      </ul>
     </>
   );
 };
